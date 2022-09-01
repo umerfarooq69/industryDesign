b5f1f16944ae56221e147429dc46eba4973a45cf
Umer farooq <45381610+umerfarooq69@users.noreply.github.com>
2022-09-01 17:17:25 +0500

    design complete

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+$('.drop-nav').click(function () {
+    $('.custom--collapse').toggleClass('show__nav');
+});
+
+
+$('.profile--dropdown').click(function () {
+    $('.customDropDown').toggleClass('show');
+});
+
+
+$('.show-sidebar-btn').click(function () {
+    $('.main-sidebar-wrap').toggleClass('show-sidebar');
+});
